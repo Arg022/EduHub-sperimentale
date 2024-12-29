1ceb2c6ceb4d23842e06cd1b4401c7201592d1db
Argeo <argeoprosperi22@gmail.com>
2024-12-29 15:15:39 +0100

    test

--- frontend/src/components/AuthLayout.tsx
+++ frontend/src/components/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "@/components/loginLayout/header"
+import { Header } from "@/components/loginLayout/Header"
 import { Footer } from "./loginLayout/Footer";
 
 const AuthLayout = () => {
